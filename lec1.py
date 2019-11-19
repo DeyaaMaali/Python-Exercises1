2f573b3cffc312d4256e40d007ab6598019d1f1f
DeyaaMaali <deyaa.maali.com>
2019-11-19 16:51:46 +0200

    Finish Ex

---
+++ lec1.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Exercieses for day 1
+
+print ("Ex1")
+
+print("Hello\n\tHello\n\t\tHello")
+
+
+
+print ("Ex2:")
+
+print ("Orange Academy\n"*20)
+
+
+
+print ("Ex3:")
+
+print('1.0102.8103.0104.2')
+
+
+
+
+print ("Ex4:")
+
+x = int(input('enter number 1'))
+
+y = int(input('enter number 2'))
+
+print('product', x*y)
+
+
+
+print ("Ex5:")
+
+sum = 0
+
+for a in range(5):
+
+     x = int(input('number'))
+
+     sum = sum + x
+
+print('avg', sum/5)
+
+
+
+print ("Ex6:")
+
+print('1- int')
+
+print('2- float')
+
+print('3- complex')
+
+print('4- str')
+
+print('5- bool')
+
+
+
+
+
+print ("Ex7:")
+
+print("1- Orange")
+
+print("2-", 1)
+
+print("3- error because x1 is not the same as X1; python is case sensitive")
+
+print("4-" , -10)
+
+
+
+print ("Ex8:")
+
+
+"""
+1- the variable shouldnt start with a number.
+2- the variable shouldnt contain a special character such as space and $.
+3- Print is not a reserved  word, print is.
+4- the double quotation should be like this: "".
+"""
+
+
+
+
+
+print ("Ex9:")
+
+print("same as Ex7.")
+
+
+
+
+
+
+
+print ("Ex10:")
+
+print("same as Ex8.")
+
+
+
+
+
+print ("Ex11:")
+
+print("Cheers, Mate!")
+
+
+
+
+
+print ("Ex12:")
+
+name= (input('Please enter your name: '))
+
+age= int(input('Please enter your age: '))
+
+year = 2019 - age + 100
+
+print('you will reach the year 100 at: ', year)
+
+
+
+
+
+print ("Ex13:")
+
+base = int(input('Please enter the base of the triangle'))
+
+height = int(input('Please enter the height of the triangle'))
+
+area= 0.5*base*height
+
+print(area)
+
+
+
+
+print ("Ex14:")
+
+print ("1- x+y:", 14)
+
+print ("2- x-y:", 8)
+
+print ("3- x*y:", 33)
+
+print ("4- x/y:", 3.67)
+
+print ("5- x//y:", 3)
+
+print ("6- x%y:",2)
+
+print ("7- abs(x*-1):", 11)
+
+print ("8- int x:", 11)
+
+print ("9- float x:", 11.0)
+
+print ("10- divmod(x,y):", "3, 2")
+
+print ("11- pow(11,3):", 1331)
+
+print ("12- x**y:" , 1331)
+
+print ("13- x>y:", "true")
+
+print ("14- x==y:", "false")
+
+print ("15- x!=y:", "true")
+
+
+
